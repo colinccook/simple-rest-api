fc3da0d0dd51b8e1f5fd2c54ece9caf469b8f574
Colin C Cook <colin@Colins-iMac.local>
2017-10-28 12:48:15 +0100

    implemented post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoClient = require('mongodb').MongoClient;
 
+// mongo db stuff
 var db;
 
 mongoClient.connect('mongodb://simple-rest-api-user:323e2428-1447-4681-bdc2-5eee4b5a9d99@ds237475.mlab.com:37475/simple-rest-api', (err, database) => {
@@ -17,4 +18,18 @@ mongoClient.connect('mongodb://simple-rest-api-user:323e2428-1447-4681-bdc2-5eee
 
 app.get('/hello', (req, res) => {
     res.send('Hello world!')
+});
+
+// http post (create)
+app.post('/', (req, res) => {
+
+    if (req.body.id) { 
+        return res.status(404).json('You cannot post an ID; the API will provide one for you');
+    }
+
+    db.collection('root').save(req.body, (err, result) => {
+        if (err) return res.status(500).json(err);
+    })
+
+    console.log('saved to database');
 });
