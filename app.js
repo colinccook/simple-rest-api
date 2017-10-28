8fcaf66a8bbfc1c21e3658effb96bce2117dc04d
Colin C Cook <colin@Colins-iMac.local>
2017-10-28 12:22:17 +0100

    installed nodemon, mongodb, created script

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
-})
+});
 
 app.get('/hello', (req, res) => {
     res.send('Hello world!')
-})
+});
